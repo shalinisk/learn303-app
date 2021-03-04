c71a2ac82d9f0114c76e253f1e077f6dc7e03f81
Shalini Kumar <shalinisanthosh92@gmail.com>
2021-03-03 18:34:54 -0600

    testing vars

--- app.py
+++ app.py
@@ -4,3 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+def index():
+    return "Welcome to the index page!"
+
+@app.route("/hello/")
+def hello():
+    return "Hello there"
+
+@app.route("/hello/<user>")
+def hello(user):
+    return f"Hello there, {user}!"
